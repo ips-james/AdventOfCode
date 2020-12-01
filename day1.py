5d6e5429d8b7d36b3d2e5c09a7db6a375bc955fd
James Stuart <ips-james@users.noreply.github.com>
2020-12-01 13:56:41 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,17 @@
+checked = []
+found_answer = False
+
+expense_list = [int(line) for line in open('day1_input.txt', 'r')]
+
+for e in expense_list:
+    for f in expense_list:
+        if e + f > 2020:
+            continue
+        if e + f == 2020:
+            print(e, f, e * f)
+
+        for g in expense_list:
+            if e + f + g > 2020:
+                continue
+            if e + f + g == 2020:
+                print(e, f, g, e * f * g)
